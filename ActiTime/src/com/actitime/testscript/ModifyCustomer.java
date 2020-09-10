a64bb8bb57365afdf86e5fb88a8f4286559547dd
Kabyakanta <kabyakantadas@gmail.com>
2020-09-10 14:15:45 +0530

    First Commit

---
+++ ActiTime/src/com/actitime/testscript/ModifyCustomer.java
@@ -0,0 +1,12 @@
+package com.actitime.testscript;
+
+import org.testng.annotations.Test;
+
+import com.actitime.generics.BaseClass;
+
+public class ModifyCustomer extends BaseClass{
+ @Test
+ public void testCreateCustomer(){
+	 
+ }
+}
